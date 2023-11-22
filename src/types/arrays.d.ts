21e92ba0667cfdf499b2a4138b3e051191b1058c
Devin Elrose <hiddnist@gmail.com>
2023-11-21 21:19:47 -0800

    fixes misnamed types

--- src/types/arrays.d.ts
+++ src/types/arrays.d.ts
@@ -2,13 +2,13 @@ export type StaticLengthArray<N extends number, T, R extends readonly T[] = []> 
   ? R
   : StaticLengthArray<N, T, [T, ...tail: R]>
 
-export type FlattenArray<T extends [][]> = T extends [infer F, ...infer R]
+export type FlattenArray<T extends any[]> = T extends [infer F, ...infer R]
   ? F extends any[]
-    ? [...Flatten<F>, ...Flatten<R>]
-    : [F, ...Flatten<R>]
+    ? [...FlattenArray<F>, ...FlattenArray<R>]
+    : [F, ...FlattenArray<R>]
   : []
 
 export type VectorArray<Size extends number> = StaticLengthArray<Size, number>
 
 export type MatrixArray<Rows extends number, Cols extends number = Rows> = StaticLengthArray<Rows, VectorArray<Cols>>
-export type FlatMatrixArray<Rows extends number, Cols extends number = Rows> = FlattenArray<Matrix<Rows, Cols>>
+export type FlatMatrixArray<Rows extends number, Cols extends number = Rows> = FlattenArray<MatrixArray<Rows, Cols>>
