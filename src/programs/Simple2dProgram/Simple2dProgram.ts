05593883a29afe0f7f77e31fbc4a3e1b3f292c71
Devin Elrose <hiddnist@gmail.com>
2023-11-21 21:19:25 -0800

    temp commit

--- src/programs/Simple2dProgram/Simple2dProgram.ts
+++ src/programs/Simple2dProgram/Simple2dProgram.ts
@@ -1,11 +1,19 @@
 import { WebGLProgramBuilder } from "../WebGLProgramBuilder"
-import positionVertexSource from "../../shaders/position.vertex.glsl"
-import fragmentShaderSource from "../../shaders/color.fragment.glsl"
+import positionVertexSource from "./position.vertex.glsl"
+import fragmentShaderSource from "./color.fragment.glsl"
 
 export class Simple2dProgram {
     public readonly program: WebGLProgram;
 
     constructor(gl: WebGLRenderingContext) {
         this.program = WebGLProgramBuilder.create(gl, positionVertexSource, fragmentShaderSource)
+    }
+    
+    public setCanvasSize(gl: WebGLRenderingContext): Simple2dProgram {
+        const width = gl.getUniformLocation(this.program, "width")
+        const height = gl.getUniformLocation(this.program, "height")
+        gl.uniform1f(width, gl.canvas.width)
+        gl.uniform1f(height, gl.canvas.height)
+        return this
     }
 }
