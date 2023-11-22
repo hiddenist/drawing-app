41d048873886cc7eee4507a606b4816aa9c2bd1f
Devin Elrose <hiddnist@gmail.com>
2023-11-22 01:13:52 -0800

    adds a github pages deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite"
 
 import plainText from "vite-plugin-plain-text"
 
+const baseUrl = process.env.BASE_URL ?? "/"
+
 export default defineConfig({
+  base: baseUrl.startsWith("/") ? baseUrl : `/${baseUrl}`,
   plugins: [plainText(["**/*.glsl"], { namedExport: false })],
 })
