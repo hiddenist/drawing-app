b4f6df9b364fde1a19118a686cfd6359ae672baf
Devin Elrose <hiddnist@gmail.com>
2023-11-23 21:46:40 -0800

    refactor drawing and add line context

--- libs/webgl/src/engine/DrawingEngine.ts
+++ libs/webgl/src/engine/DrawingEngine.ts
@@ -1,6 +1,8 @@
 import { ColorContext } from "./ColorContext"
 import { LineDrawingProgram, DrawLineOptions, DrawType } from "../programs/LineDrawingProgram"
 import { BaseDrawingEngine } from "./BaseDrawingEngine"
+import { LineContext } from "./LineContext"
+import { VectorArray } from "../types/arrays"
 
 interface AvailablePrograms {
   lineDrawing: LineDrawingProgram
@@ -10,6 +12,8 @@ export class DrawingEngine extends BaseDrawingEngine<AvailablePrograms> {
   protected programs: AvailablePrograms
   protected context = {
     color: new ColorContext(),
+    line: new LineContext(),
+    isDrawing: false,
   }
 
   constructor(canvas: HTMLCanvasElement) {
@@ -36,5 +40,32 @@ export class DrawingEngine extends BaseDrawingEngine<AvailablePrograms> {
   public clearCanvas() {
     this.gl.clearColor(0, 0, 0, 0)
     this.gl.clear(this.gl.COLOR_BUFFER_BIT)
+  }
+
+  public setPressed(pressed: boolean, position: VectorArray<2>) {
+    this.context.isDrawing = pressed
+    if (pressed) {
+      this.context.line.addPoint(position)
+      this.updateDrawing(this.context.line.flat)
+    } else {
+      this.commitDrawing()
+      this.context.line.clearCurrentSegment()
+    }
+  }
+
+  protected commitDrawing() {
+    if (this.context.line.currentSegment.length > 0) {
+      this.drawLine(this.context.line.flat, { drawType: DrawType.STATIC_DRAW })
+      // todo: add to a history context
+      // todo: copy to a persistent canvas
+    }
+  }
+
+  public setPosition(position: VectorArray<2>) {
+    if (this.context.isDrawing) {
+      // todo: remove line segments that are outside the canvas
+      this.context.line.addPoint(position)
+      this.updateDrawing(this.context.line.flat)
+    }
   }
 }
