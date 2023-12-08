2ffbd3ef8fb22b57f01802aad2e6300f2b6a7e5d
Devin Elrose <hiddnist@gmail.com>
2023-12-08 02:25:33 -0800

    idk

--- libs/webgl/src/engine/Layer.ts
+++ libs/webgl/src/engine/Layer.ts
@@ -12,7 +12,7 @@ export class Layer {
   public constructor(
     gl: WebGLRenderingContext,
     protected _settings: LayerSettings = {},
-    fromImage: null | Image = null
+    fromImage: Image | null = null
   ) {
     this._gl = gl
 
@@ -33,11 +33,8 @@ export class Layer {
     gl.bindTexture(gl.TEXTURE_2D, texture)
 
     const { width, height } = this.gl.canvas
-    if (image) {
+
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, image)
-    } else {
-      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, null)
-    }
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
