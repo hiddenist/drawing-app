ccd4b8939efe37b685659725216ea9ea74f5b453
Devin Elrose <hiddnist@gmail.com>
2023-12-13 23:39:00 -0800

    comment out line unbinding the texture
    attempting a fix for #44

--- libs/drawing-engine/src/engine/Layer.ts
+++ libs/drawing-engine/src/engine/Layer.ts
@@ -52,7 +52,7 @@ export class Layer {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-    gl.bindTexture(gl.TEXTURE_2D, null)
+    // gl.bindTexture(gl.TEXTURE_2D, null)
     return texture
   }
 
